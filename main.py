9b45937ac7f965196f8e7165d631242050a5ff64
Ahmad Zaiour <mido@MacBook-Air-von-Mido.local>
2024-12-14 20:35:54 +0100

    FE Design

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
 import sys
 import os
-
-# Füge das Projektverzeichnis zu sys.path hinzu
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, PROJECT_ROOT)
-
 from flask import Flask, render_template, session, request
 import pandas as pd
 from io import BytesIO
 from backend.time_slot_input.extract_syllabus_info import GetSallybusInfo
 from backend.tor_input.pdf_to_text import GetCompSubsFromInput
 
-app = Flask(__name__) 
-#TODO change to secret key
+# Füge das Projektverzeichnis zu sys.path hinzu
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, PROJECT_ROOT)
+
+app = Flask(__name__)
+# TODO: Ändere den secret key
 app.secret_key = 'your_secret_key'
 
-@app.route('/')  
+@app.route('/')
 def home():
-    #Reset all previus stored data
+    # Reset all previously stored data
     session = None
     return render_template('landing_page.html')
 
@@ -32,20 +31,20 @@ def upload_data():
 
     try:
         syllabus_data_df = pd.read_excel(BytesIO(syllabus_file.read()))
-        #sallybus = GetSallybusInfo().from_myStudy_exel_export(syllabus_data_df)
-        #session['sallybus'] = sallybus    
+        # sallybus = GetSallybusInfo().from_myStudy_exel_export(syllabus_data_df)
+        # session['sallybus'] = sallybus
     except Exception as e:
-        print(f'extracting information from sallybus was not possible {e}')
+        print(f'extracting information from syllabus was not possible: {e}')
 
     try:
         tor_pdf_data = BytesIO(tor_file.read())
         finished_comps = GetCompSubsFromInput().getCompSubsFromTOR(tor_pdf_data)
         session['finished_comps'] = finished_comps
-
     except Exception as e:
-        print(f'extracting information from TOR was not possible {e}')
+        print(f'extracting information from TOR was not possible: {e}')
 
     return render_template('select_interests_page.html')
 
 if __name__ == '__main__':
+    # Starte den Flask-Server
     app.run(debug=True)
